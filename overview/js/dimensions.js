aef84fec4dedc28174c65dd590b631d2c36925d7
Leslie Dao <l.dao@hotmail.com>
2016-06-06 16:35:18 +0200

    extra properties voor floor1.html toegevoegd

--- overview/js/dimensions.js
+++ overview/js/dimensions.js
@@ -13,34 +13,53 @@ var svg = {
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.right + ")")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+    f1: d3.select("#f1-div").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 };
 
 var x = {
     general: d3.time.scale()
-                .range([0, width])
+                .range([0, width]),
+    f1: d3.time.scale()
+            .range([0, width])
 };
 
 var y = {
     general: d3.scale.linear()
-                .range([height, 0])
+                .range([height, 0]),
+    f1: d3.scale.linear()
+            .range([height, 0])
 };
 
 var xAxis = {
     general: d3.svg.axis()
                 .scale(x.general)
                 .orient("bottom")
-                .ticks(14)
+                .ticks(14),
+    f1: d3.svg.axis()
+            .scale(x.f1)
+            .orient("bottom")
+            .ticks(14)
 };
 
 var yAxis = {
     general: d3.svg.axis()
                 .scale(y.general)
-                .orient("left")
+                .orient("left"),
+    f1: d3.svg.axis()
+            .scale(y.f1)
+            .orient("left")
 };
 
 var line = {
     general: d3.svg.line()
                 .x(function(d) {return x.general(d.timestamp);})
-                .y(function(d) {return y.general(d.val);})
+                .y(function(d) {return y.general(d.val);}),
+    f1: d3.svg.line()
+            .x(function(d) {return x.f1(d.timestamp);})
+            .y(function(d) {return y.f1(d.val);})
 };
